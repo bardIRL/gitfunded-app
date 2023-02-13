ba8d76388494fd59c604f0a764fbf69ac6ea59ce
Alyjah-Luna <alyjahluna@gmail.com>
2023-02-13 17:25:11 -0500

    Fixed merge conflicts & added create campaign functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('campaigns/', views.campaigns_index, name='index'),
   path('campaigns/<int:campaign_id>/', views.campaigns_detail, name='detail'),
+  path('campaigns/create/', views.CampaignCreate.as_view(), name='campaigns_create'),
   path('campaigns/<int:pk>/delete/', views.CampaignDelete.as_view(), name='campaigns_delete'),
 ]
