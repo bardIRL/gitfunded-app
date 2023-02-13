8f364539b17118de692a087f4ecbac81d2e56c88
Alyjah-Luna <alyjahluna@gmail.com>
2023-02-13 13:57:08 -0500

    fixed donations to donation

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Campaign, Donations
+from .models import Campaign, Donation
 
 # Register your models here.
 admin.site.register(Campaign)
-admin.site.register(Donations)
+admin.site.register(Donation)
