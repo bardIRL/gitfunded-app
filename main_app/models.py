8d2d7d8541976519b5f9aa364d423cf449727f15
bardIRL <115611931+bardIRL@users.noreply.github.com>
2023-02-13 10:42:19 -0800

    defines Campaign model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Campaign(models.Model):
+    title = models.CharField(max_length=250)
+    category = models.CharField(max_length=100)
+    goal = models.IntegerField()
+    goal_date = models.DateField()
+    about = models.TextField(max_length=1000)
+    links = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
